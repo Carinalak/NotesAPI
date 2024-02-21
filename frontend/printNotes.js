7bed66b51445e60ce548f8d8e3b17fd3ca1aed25
Carinalak <carina.lakosil@gmail.com>
2024-02-21 13:28:14 +0100

    printnote fortsättning

--- frontend/printNotes.js
+++ frontend/printNotes.js
@@ -1,32 +1,6 @@
 import deleteNote from "./deleteNote.js";
 
 
-
-// ------- SKRIVER UT DET DOKUMENT SOM MAN KLICKAR PÅ --------- //
-
-function openNote() {
-    fetch("http://localhost:3000/notes/" + id)
-
-    .then(res => res.json())
-    .then(data => {
-        data.map(notes => {
-            let h3 = document.createElement("h3");
-            h3.innerText = notes.name;
-
-            let div = document.createElement("div");
-            div.innerText = notes.note;
-
-           // NoteName.addEventListener("click", () => {
-
-            //    console.log(notes.note);
-           //});
-            noteName.appendChild(h3);
-            noteText.appendChild(div);
-        })
-    })
-    
-}
-
     
 // ------- SKRIVER UT ALLA NOTES TILL EN LISTA----- //
 
@@ -44,6 +18,8 @@ export default function printNotes() {
             let deleteIcon = document.createElement("img");
             let notesId = note.id;
 
+// ---------------- DELETE EN NOTE --------------------- //
+
             deleteIcon.src = "img/deleteIcon.png";
             deleteIcon.alt = "Ta bort";
             deleteIcon.classList.add("delete-icon");
@@ -55,13 +31,15 @@ export default function printNotes() {
 
             li.appendChild(deleteIcon);
 
+// ------------------- SLUT DELETE ------------------------ //
+
             let textNode = document.createTextNode(note.name);
             li.appendChild(textNode);
 
             notesList.appendChild(li);
 
             li.addEventListener("click", () => {
-
+            openNote();
             console.log("Click!");
 
             });
@@ -73,32 +51,24 @@ export default function printNotes() {
 };
 
 
-
-
-
-/*
 // ------- SKRIVER UT DET DOKUMENT SOM MAN KLICKAR PÅ --------- //
-function printNote() {
-    fetch("http:localhost:3000/notes/:id")
+
+function openNote() {
+    fetch("http://localhost:3000/notes/" + id)
+
+    .then(res => res.json())
     .then(res => res.json())
     .then(data => {
+        data.forEach(notes => {
+            let h3 = document.createElement("h3");
+            h3.innerText = notes.name;
 
+            let div = document.createElement("div");
+            div.innerText = notes.note;
 
-        data.map(notes => {
-            let NoteName = document.createElement("h3");
-            let NoteText = document.createElement("div");
-            NoteName.innerText = notes.name;
-            NoteText.innerText = notes.note;
-
-            NoteName.addEventListener("click", () => {
-
-                openNote(notes.note)
-                console.log(notes.note);
-            });
-            openNote(notes.id);
-        })
-    })
+            document.body.appendChild(h3);
+            document.body.appendChild(div);
+        });
+    });
 }
-*/
-
 
